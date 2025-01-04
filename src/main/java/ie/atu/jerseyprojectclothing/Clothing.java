9018a65bace767592119e08870072bd02c2bd958
ThomasGallagher1 <g00415457@atu.ie>
2025-01-04 16:50:53 +0000

    Made clothing talk to item application to print when item is found

--- src/main/java/ie/atu/jerseyprojectclothing/Clothing.java
+++ src/main/java/ie/atu/jerseyprojectclothing/Clothing.java
@@ -1,17 +1,43 @@
 package ie.atu.jerseyprojectclothing;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
-@Data
+
 @Entity
+@Table(name = "clothing")
 public class Clothing {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long ClothingId;
-    private String ClothingType;
+    @Column(name = "clothing_id")
+    private Long clothingId;
+    @Column(name = "clothing_type")
+    private String clothingType;
+    @Column(name = "team_id")
     private Long teamId;
+    public Long getClothingId() {
+        return clothingId;
+    }
+
+    public void setClothingId(Long clothingId) {
+        this.clothingId = clothingId;
+    }
+
+    public String getClothingType() {
+        return clothingType;
+    }
+
+    public void setClothingType(String clothingType) {
+        this.clothingType = clothingType;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+
 }
--- src/main/java/ie/atu/jerseyprojectclothing/ClothingController.java
+++ src/main/java/ie/atu/jerseyprojectclothing/ClothingController.java
@@ -2,12 +2,18 @@ package ie.atu.jerseyprojectclothing;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.nio.ReadOnlyBufferException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/clothing")
@@ -18,6 +24,17 @@ public class ClothingController {
 
     @GetMapping("/team/{teamId}")
     public List<Clothing> getClothingByTeams(@PathVariable Long teamId){
-        return clothingRepository.findByTeamId(teamId);
+        List<Clothing> clothing = clothingRepository.findByTeamId(teamId);
+        return clothing;
+    }
+
+    @GetMapping("/{clothingId}")
+    public ResponseEntity<Map<String, Object>> getClothingDetails(@PathVariable Long clothingId){
+        Clothing clothing = clothingRepository.findById(clothingId).orElseThrow(() -> new RuntimeException("Clothing not found"));
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("clothingId", clothing.getClothingId());
+        response.put("clothingName", clothing.getClothingType());
+        return ResponseEntity.ok(response);
     }
 }
